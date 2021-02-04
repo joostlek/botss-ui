4cbbd8150ecf1e08db7d91f456727a5678a42784
joostlek <joostlek@outlook.com>
2021-02-04 15:04:55 +0100

    Add whatsapp share button

--- src/views/dashboard/teams/InviteMembers.vue
+++ src/views/dashboard/teams/InviteMembers.vue
@@ -3,6 +3,10 @@
   <h2>Nodig mensen uit bij {{team.name}}.</h2>
   <p>Deel de volgende code met hun:</p>
   <h1>{{team.joinCode}}</h1>
+  <h2>Deel deze code:</h2>
+  <v-btn icon color="green" dark @click="sendWhatsapp">
+    <v-icon>mdi-whatsapp</v-icon>
+  </v-btn>
 </div>
 </template>
 
@@ -14,6 +18,16 @@ name: "InviteMembers",
   }),
   mounted() {
     this.team = JSON.parse(localStorage.getItem('team'));
+  },
+  methods: {
+    sendWhatsapp() {
+      window.open(this.whatsAppLink, '_blank');
+    }
+  },
+  computed: {
+    whatsAppLink() {
+      return `https://wa.me/?text=Wil jij in mijn groepje bij de Battle of Student Societies? Mijn code is ${this.team.joinCode}.`;
+    }
   }
 }
 </script>
