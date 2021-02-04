7e1264855b8837afacc75e7c995d97577be254b3
joostlek <joostlek@outlook.com>
2021-02-04 14:56:28 +0100

    Add validations to create team

--- src/views/dashboard/teams/CreateTeam.vue
+++ src/views/dashboard/teams/CreateTeam.vue
@@ -2,15 +2,29 @@
   <div>
     <h2>Maak een team aan</h2>
     <p>Om mee te kunnen doen met de teamcompetitie moet je deelnemen aan een team, je kan er zelf eentje aanmaken maar je kan er ook eentje joinen.</p>
-    <v-text-field
-        label="Team naam"
-        v-model="name"
-      ></v-text-field>
-    <v-btn
-        @click="createTeam"
-      color="primary">
-      Maak team
-    </v-btn>
+    <v-form
+        v-model="valid"
+        ref="form"
+        lazy-validation
+    >
+      <v-row>
+        <v-col
+          cols="12"
+          md="6">
+          <v-text-field
+              label="Team naam"
+              v-model="name"
+              :rules="validationRule"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-btn
+          :disabled="!valid"
+          @click="createTeam"
+          color="primary">
+        Maak team
+      </v-btn>
+    </v-form>
   </div>
 </template>
 
@@ -18,12 +32,21 @@
 export default {
   name: "CreateTeam",
   data: () => ({
-    name: ""
+    name: "",
+    valid: true,
+    validationRule: [
+      v => v.length < 50 || 'De teamnaam moet korter zijn dan 50 tekens.',
+      v => v.length !== 0 || 'De teamnaam mag niet leeg zijn.'
+    ],
   }),
   methods: {
     async createTeam() {
-      const res = await this.$api.post("/teams", {name: this.name});
-      localStorage.setItem("teamId", res.data.id);
+      this.validate();
+      await this.$api.post("/teams", {name: this.name});
+      this.$eventBus.$emit('updateUserTeam')
+    },
+    validate() {
+      this.$refs.form.validate();
     }
   }
 }
