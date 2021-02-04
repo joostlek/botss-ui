b6ea9b32560529183a60ca050dceaf9c83a963b1
joostlek <joostlek@outlook.com>
2021-02-04 14:49:45 +0100

    Add join team validations

--- src/views/dashboard/teams/JoinTeam.vue
+++ src/views/dashboard/teams/JoinTeam.vue
@@ -2,9 +2,28 @@
 <div>
   <h2>Neem deel aan een team</h2>
   <p>Om mee te doen met een team moet je hier de 6 cijferige code invullen die je van het team gekregen hebt!</p>
-  <v-text-field
-    v-model="joinCode"></v-text-field>
+  <v-alert
+      v-if="!!error"
+    type="error">
+    {{error}}
+  </v-alert>
+  <v-row>
+    <v-col
+      cols="12"
+      md="6">
+      <v-form
+          ref="form"
+          v-model="valid"
+          lazy-validation
+      >
+        <v-text-field
+            :rules="validationRule"
+            v-model="joinCode"></v-text-field>
+      </v-form>
+    </v-col>
+  </v-row>
   <v-btn
+      :disabled="!valid"
     color="primary"
     @click="joinTeam"
     >
@@ -17,13 +36,30 @@
 export default {
   name: "JoinTeam",
   data: () => ({
-    joinCode: ''
+    joinCode: '',
+    error: null,
+    validationRule: [
+        v => v.length === 6 || 'De code moet 6 tekens lang zijn.'
+    ],
+    valid: true
   }),
   methods: {
     async joinTeam() {
-      await this.$api.post('/teams/join', {joinCode: this.joinCode});
-      this.$eventBus.$emit("updateUserTeam");
-      await this.$router.push({name: "TeamInfo"})
+      this.validate();
+      try {
+        await this.$api.post('/teams/join', {joinCode: this.joinCode});
+        this.$eventBus.$emit("updateUserTeam");
+        await this.$router.push({name: "TeamInfo"})
+      } catch (e) {
+        if (e.response.status === 404) {
+          this.error = "Team niet gevonden."
+        } else {
+          this.error = "Er is iets misgegaan, probeer het opnieuw."
+        }
+      }
+    },
+    validate() {
+      this.$refs.form.validate();
     }
   }
 }
